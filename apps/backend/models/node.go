1f3e8009fe4a3f7e265be0c5ef139167c925e056
mel <mcmelon@nodiumhosting.com>
2025-06-02 00:41:58 +0200

    feat: update node model

--- apps/backend/models/node.go
+++ apps/backend/models/node.go
@@ -10,4 +10,7 @@ type Node struct {
 	Location    Location     `json:"location"`
 	Servers     []Server     `gorm:"foreignKey:NodeID" json:"servers"`
 	Allocations []Allocation `gorm:"foreignKey:NodeID" json:"allocations"`
+	MaxRAM      uint         `gorm:"not null" json:"max_ram"`     // Maximum RAM in MB
+	MaxStorage  uint         `gorm:"not null" json:"max_storage"` // Maximum Storage in MB
+	AuthToken   string       `gorm:"not null" json:"auth_token"`  // Encrypted authentication token for the node
 }
