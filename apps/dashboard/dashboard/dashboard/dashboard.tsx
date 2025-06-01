cb98d97c3d2f87caa2f99c95400aac44fdf1393e
mel <mcmelon@nodiumhosting.com>
2025-06-02 01:17:50 +0200

    chore: I swear this is the last time I switch the frontend framework... probably

--- apps/dashboard/dashboard/dashboard/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import {Route, Routes} from 'react-router-dom';
-
-export function Dashboard() {
-    return (
-        <Routes>
-            <Route path="/" element={<h1>Hello World!</h1>}/>
-        </Routes>
-    );
-}
