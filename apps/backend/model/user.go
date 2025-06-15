04a49e2434f67e2c0e65977e43355dd965bff4ec
mel <mcmelon@nodiumhosting.com>
2025-06-15 23:58:37 +0200

    feat: change user db model to include mfa needed bool

--- apps/backend/model/user.go
+++ apps/backend/model/user.go
@@ -9,8 +9,9 @@ type User struct {
 	Email        string       `gorm:"uniqueIndex;not null" json:"email"`
 	PasswordHash string       `gorm:"not null" json:"-"`
 	PasswordSalt string       `gorm:"not null" json:"-"`
+	MFANeeded    bool         `gorm:"not null" json:"mfa_needed"`
+	MFAMethods   []UserMFA    `gorm:"foreignKey:UserID" json:"mfa_methods"`
 	RefreshToken string       `gorm:"not null" json:"-"`
 	OwnedServers []Server     `gorm:"foreignKey:OwnerID" json:"owned_servers"`
 	Servers      []ServerUser `gorm:"foreignKey:UserID" json:"servers"`
-	MFAMethods   []UserMFA    `gorm:"foreignKey:UserID" json:"mfa_methods"`
 }
