250747cfdb739643a65cfd66829fa1bb44f85f80
Viliam-Grinc <viliam.grinc@gmail.com>
2025-06-16 15:02:31 +0200

    refactor: managing mockServers internaly

--- apps/dashboard/app/routes/dashboard/index.tsx
+++ apps/dashboard/app/routes/dashboard/index.tsx
@@ -8,97 +8,8 @@ import ServerCardGrid, { type Server } from "./ServerCardGrid";
 
 import useDashboard from "./useDashboard";
 
-const mockServers: Server[] = [
-  {
-    id: "1",
-    name: "Minecraft SMP",
-    status: ServerStatusType.ONLINE,
-    description: "Survival multiplayer server",
-    cpuUsage: 45,
-    memoryUsage: {
-      used: 2048 * 1024,
-      total: 4096 * 1024,
-    },
-    game: "Minecraft",
-    players: {
-      online: 12,
-      max: 50,
-    },
-    ip: "192.168.1.1",
-    port: 25565,
-  },
-  {
-    id: "2",
-    name: "Web Server",
-    status: ServerStatusType.STARTING,
-    description: "NGINX web server",
-    cpuUsage: 12,
-    memoryUsage: {
-      used: 512 * 1024,
-      total: 1024 * 1024,
-    },
-    game: "Nginx",
-    ip: "192.168.1.6",
-    port: 80,
-  },
-  {
-    id: "3",
-    name: "Terraria Adventure",
-    status: ServerStatusType.OFFLINE,
-    description: "Terraria modded adventure server",
-    cpuUsage: 0,
-    memoryUsage: {
-      used: 0,
-      total: 2048 * 1024,
-    },
-    game: "Terraria",
-    players: {
-      online: 0,
-      max: 16,
-    },
-    ip: "192.168.1.3",
-    port: 7777,
-  },
-  {
-    id: "4",
-    name: "Rust Survival",
-    status: ServerStatusType.STOPPING,
-    description: "Rust survival server",
-    cpuUsage: 30,
-    memoryUsage: {
-      used: 1024 * 1024,
-      total: 2048 * 1024,
-    },
-    game: "Rust",
-    players: {
-      online: 5,
-      max: 100,
-    },
-    ip: "192.168.1.23",
-    port: 28015,
-  },
-  {
-    id: "5",
-    name: "ARK: Survival Evolved",
-    status: ServerStatusType.UNKNOWN,
-    description: "ARK survival server",
-    cpuUsage: 60,
-    memoryUsage: {
-      used: 3072 * 1024,
-      total: 8192 * 1024,
-    },
-    game: "ARK",
-    players: {
-      online: 20,
-      max: 70,
-    },
-    ip: "192.168.1.13",
-    port: 7778,
-  },
-];
-
 export default function DashboardOverviewPage() {
-  const dashboardData = useDashboard({ mockServers });
+  const dashboardData = useDashboard();
 
   const overviewCardsData: OverviewCardProps[] = [
     {
@@ -157,7 +68,7 @@ export default function DashboardOverviewPage() {
 
       <OverviewCardGrid cards={overviewCardsData} />
 
-      <ServerCardGrid servers={mockServers} />
+      <ServerCardGrid servers={dashboardData.mockServers} />
     </div>
   );
 }
