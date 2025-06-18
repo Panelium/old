088b7f30c81a29c7b7057de28e6f299af32bfabd
mel <mcmelon@nodiumhosting.com>
2025-06-18 23:22:26 +0200

    refactor: move jwt helpers to its own package and split between backend and common

---
+++ apps/backend/internal/security/jwt/jwt.go
@@ -0,0 +1,29 @@
+package jwt
+
+func CreateMFASessionToken(userID string, sessionID string) (string, error) {
+	return "", nil
+}
+
+func CreateAccessToken(userID string, sessionID string) (string, error) {
+	return "", nil
+}
+
+func RefreshAccessToken(accessToken string) (string, error) {
+	return "", nil
+}
+
+func CreateRefreshToken(userID string, sessionID string) (string, error) {
+	return "", nil
+}
+
+func VerifyMFASessionToken(token string) (string, error) {
+	return "", nil
+}
+
+func VerifyAccessToken(token string) (string, error) {
+	return "", nil
+}
+
+func VerifyRefreshToken(token string) (string, error) {
+	return "", nil
+}
