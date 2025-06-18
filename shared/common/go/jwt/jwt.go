eab3efe4aba24d3dc6b9040bd5e6a27f371e55c8
mel <mcmelon@nodiumhosting.com>
2025-06-19 00:11:21 +0200

    feat: create a JWT Claims struct type

--- shared/common/go/jwt/jwt.go
+++ shared/common/go/jwt/jwt.go
@@ -1,1 +1,18 @@
 package jwt
+
+type Claims struct {
+	IssuedAt   int64  `json:"iat"`
+	Expiration int64  `json:"exp"`
+	Subject    string `json:"sub"` // User ID
+	Audience   string `json:"aud"` // Session ID
+	Issuer     string `json:"iss"` // Issuer (backend/daemon)
+	TokenType  string `json:"typ"` // Token type (e.g., "access", "refresh", "mfa")
+}
+
+func CreateJWT(claims Claims, secret string) string {
+	return ""
+}
+
+func VerifyJWT(token string, secret string) (*Claims, error) {
+	return nil, nil
+}
