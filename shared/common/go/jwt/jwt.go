3c6b0c5893045f4a78fd5c2057e897da9d3c1fe6
mel <mcmelon@nodiumhosting.com>
2025-06-19 00:32:04 +0200

    feat: add optional jti and nbf to Claims and made sub optional

--- shared/common/go/jwt/jwt.go
+++ shared/common/go/jwt/jwt.go
@@ -1,12 +1,14 @@
 package jwt
 
 type Claims struct {
-	IssuedAt   int64  `json:"iat"`
-	Expiration int64  `json:"exp"`
-	Subject    string `json:"sub"` // User ID
-	Audience   string `json:"aud"` // Session ID
-	Issuer     string `json:"iss"` // Issuer (backend/daemon)
-	TokenType  string `json:"typ"` // Token type (e.g., "access", "refresh", "mfa")
+	IssuedAt   int64   `json:"iat"`           // Issued at time
+	NotBefore  *int64  `json:"nbf"`           // Not before time (optional as not all tokens need it)
+	Expiration int64   `json:"exp"`           // Expiration time
+	Subject    *string `json:"sub"`           // User ID (optional as MFA session tokens should not include this)
+	Audience   string  `json:"aud"`           // Session ID
+	Issuer     string  `json:"iss"`           // Issuer (backend/daemon)
+	TokenType  string  `json:"typ"`           // Token type (e.g., "access", "refresh", "mfa")
+	ID         *string `json:"jti,omitempty"` // JWT ID - unique identifier for the token (optional as not all tokens need it)
 }
 
 func CreateJWT(claims Claims, secret string) string {
