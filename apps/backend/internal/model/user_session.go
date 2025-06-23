6e5495a780fabf98af81334cc78c5bcfe71c605b
mel <mcmelon@nodiumhosting.com>
2025-06-23 10:38:06 +0200

    feat: store latest access JTI in UserSession db

--- apps/backend/internal/model/user_session.go
+++ apps/backend/internal/model/user_session.go
@@ -6,6 +6,7 @@ type UserSession struct {
 	SessionID  string    `gorm:"uniqueIndex;not null" json:"session_id"`
 	UserID     string    `gorm:"index;not null" json:"user_id"`
 	User       User      `json:"user"`
+	AccessJTI  string    `gorm:"uniqueIndex;not null" json:"-"`
 	RefreshJTI string    `gorm:"uniqueIndex;not null" json:"-"`
-	Expiration time.Time `gorm:"not null" json:"expiration"` // current JWT expiration
+	Expiration time.Time `gorm:"not null" json:"expiration"` // same as current refresh JWT expiration
 }
