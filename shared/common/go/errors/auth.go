61554d63c95a3a12a885cd683008fbc094bfbb57
mel <mcmelon@nodiumhosting.com>
2025-06-24 05:04:03 +0200

    refactor: add new error (session not found)

--- shared/common/go/errors/auth.go
+++ shared/common/go/errors/auth.go
@@ -5,3 +5,4 @@ import "errors"
 var UserNotFound = errors.New("user not found")
 var InvalidCredentials = errors.New("invalid credentials")
 var SessionCreationFailed = errors.New("session creation failed")
+var SessionNotFound = errors.New("session not found")
