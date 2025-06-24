89f23dba62a8869dc5428632b2c4083dcd78e30d
mel <mcmelon@nodiumhosting.com>
2025-06-24 05:03:31 +0200

    refactor: make session management easier

--- apps/backend/internal/security/session/session.go
+++ apps/backend/internal/security/session/session.go
@@ -2,52 +2,100 @@ package session
 
 import (
 	stdErrors "errors"
-	"panelium/backend/internal/errors"
 	"panelium/backend/internal/global"
 	"panelium/backend/internal/model"
+	"panelium/common/errors"
 	"panelium/common/id"
-	"panelium/common/jwt"
 	"time"
 )
 
-func CreateSession(uid string) (sessionId string, refreshToken string, err error) {
+// TODO: refactor this file
+
+func CreateSession(uid string) (sessionId string, refreshToken string, accessToken string, err error) {
 	sessionId, err = id.New()
 	if err != nil {
-		return "", "", stdErrors.Join(err, errors.SessionCreationFailed)
-	}
-	jti, err := id.New()
-	if err != nil {
-		return "", "", stdErrors.Join(err, errors.SessionCreationFailed)
+		return "", "", "", stdErrors.Join(err, errors.SessionCreationFailed)
 	}
 
-	claims := jwt.Claims{
-		IssuedAt:   time.Now().Unix(),
-		NotBefore:  nil,
-		Expiration: time.Now().Add(time.Hour * 24).Unix(), // TODO: this needs more thought, perhaps config?
-		Subject:    &uid,
-		Audience:   sessionId,
-		Issuer:     "backend", // TODO: we might want to make this shorter
-		TokenType:  "refresh", // TODO: we might want to make this shorter
-		JTI:        &jti,
-	}
+	timeNow := time.Now()
 
-	refreshToken, err = jwt.CreateJWT(claims, global.JWTSecret)
+	refreshToken, refreshJTI, refreshExpiration, err := CreateRefreshToken(timeNow, sessionId, uid)
 	if err != nil {
-		return "", "", stdErrors.Join(err, errors.SessionCreationFailed)
+		return "", "", "", stdErrors.Join(err, errors.SessionCreationFailed)
+	}
+	accessToken, accessJTI, _, err := CreateAccessToken(timeNow, sessionId, uid)
+	if err != nil {
+		return "", "", "", stdErrors.Join(err, errors.SessionCreationFailed)
 	}
 
 	result := global.DB.Model(model.UserSession{}).Create(&model.UserSession{
 		SessionID:  sessionId,
 		UserID:     uid,
-		RefreshJTI: jti,
+		AccessJTI:  accessJTI,
+		RefreshJTI: refreshJTI,
+		Expiration: refreshExpiration,
 	})
 
+	if result.Error != nil {
+		return "", "", "", stdErrors.Join(result.Error, errors.SessionCreationFailed)
+	}
+	if result.RowsAffected == 0 {
+		return "", "", "", errors.SessionCreationFailed
+	}
+
+	return sessionId, refreshToken, accessToken, nil
+}
+
+func RefreshSession(sessionId string) (refreshToken string, accessToken string, err error) {
+	// TODO: need to review this, might have to change the logic
+	result := global.DB.Model(model.UserSession{}).Where("session_id = ?", sessionId).First(&model.UserSession{})
+	if result.Error != nil {
+		return "", "", stdErrors.Join(result.Error, errors.SessionNotFound)
+	}
+	if result.RowsAffected == 0 {
+		return "", "", errors.SessionNotFound
+	}
+
+	session := &model.UserSession{}
+	if err := result.Scan(session); err.Error != nil {
+		return "", "", stdErrors.Join(err.Error, errors.SessionNotFound)
+	}
+	uid := session.UserID
+
+	timeNow := time.Now()
+
+	refreshToken, refreshJTI, refreshExpiration, err := CreateRefreshToken(timeNow, sessionId, uid)
+	if err != nil {
+		return "", "", stdErrors.Join(err, errors.SessionCreationFailed)
+	}
+	accessToken, accessJTI, _, err := CreateAccessToken(timeNow, sessionId, uid)
+	if err != nil {
+		return "", "", stdErrors.Join(err, errors.SessionCreationFailed)
+	}
+
+	result = global.DB.Model(model.UserSession{}).Where("session_id = ?", sessionId).Updates(&model.UserSession{
+		AccessJTI:  accessJTI,
+		RefreshJTI: refreshJTI,
+		Expiration: refreshExpiration,
+	})
 	if result.Error != nil {
 		return "", "", stdErrors.Join(result.Error, errors.SessionCreationFailed)
 	}
 	if result.RowsAffected == 0 {
-		return "", "", errors.SessionCreationFailed
+		return "", "", errors.SessionNotFound
 	}
 
-	return sessionId, refreshToken, nil
+	return refreshToken, accessToken, nil
+}
+
+func DeleteSession(sessionId string) error {
+	result := global.DB.Model(model.UserSession{}).Where("session_id = ?", sessionId).Delete(&model.UserSession{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.SessionNotFound
+	}
+
+	return nil
 }
