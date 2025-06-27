89b411f02cb32e9a31ce6b82ee54d9686e4a8fee
mel <mcmelon@nodiumhosting.com>
2025-06-27 14:24:15 +0200

    feat: add validation package with username and password regex

---
+++ apps/backend/internal/global/validation.go
@@ -0,0 +1,50 @@
+package global
+
+import (
+	"github.com/go-playground/validator/v10"
+	"regexp"
+	"sync"
+)
+
+var (
+	initOnce            sync.Once
+	validate            *validator.Validate
+	usernameRegexString = `^[a-zA-Z0-9][a-zA-Z0-9_.-]{1,30}[a-zA-Z0-9]$` // Username must start and end with an alphanumeric character, can contain alphanumeric characters, underscores, hyphens, and dots in between, and must be between 3 and 32 characters long.
+	usernameRegex       = regexp.MustCompile(usernameRegexString)
+	passwordRegexString = `^[A-Za-z0-9]{8,}$` // TODO: actual password validation
+	passwordRegex       = regexp.MustCompile(passwordRegexString)
+)
+
+func InitValidator() error {
+	var err error
+
+	initOnce.Do(func() {
+		validate = validator.New(validator.WithRequiredStructEnabled())
+
+		err = validate.RegisterValidation("username", func(fl validator.FieldLevel) bool {
+			return usernameRegex.MatchString(fl.Field().String())
+		})
+		if err != nil {
+			return
+		}
+
+		err = validate.RegisterValidation("password", func(fl validator.FieldLevel) bool {
+			return passwordRegex.MatchString(fl.Field().String())
+		})
+		if err != nil {
+			return
+		}
+	})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ValidatorInstance() *validator.Validate {
+	if validate == nil {
+		panic("validator not initialized, call InitValidator() first")
+	}
+	return validate
+}
