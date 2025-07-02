a2a83f02fbf128d6d32fd1597a0ea8f789260bed
mel <mcmelon@nodiumhosting.com>
2025-07-02 14:37:43 +0200

    feat: add secrets to daemon config

--- apps/daemon/internal/config/config.go
+++ apps/daemon/internal/config/config.go
@@ -11,13 +11,12 @@ import (
 	"sync"
 )
 
-// TODO: NodeJTI and BackendToken
-
 const BasePath = "/etc/panelium/daemon"
 
 // File names
 const gitignoreFileName = ".gitignore"
 const configFileName = "config.json"
+const secretsFileName = "secrets.json"
 const backendJWTPublicKeyFileName = "backend_jwt_public_key.pem"
 const jwtPrivateKeyFileName = "jwt_private_key.pem"
 const jwtPublicKeyFileName = "jwt_public_key.pem"
@@ -26,6 +25,7 @@ const DatabaseFileName = "daemon.db"
 // File Locations
 const gitignoreLocation = BasePath + "/" + gitignoreFileName
 const configLocation = BasePath + "/" + configFileName
+const secretsLocation = BasePath + "/" + secretsFileName
 const backendJWTPublicKeyLocation = BasePath + "/" + backendJWTPublicKeyFileName
 const jwtPrivateKeyLocation = BasePath + "/" + jwtPrivateKeyFileName
 const jwtPublicKeyLocation = BasePath + "/" + jwtPublicKeyFileName
@@ -36,6 +36,7 @@ const gitignoreContent = "*.db\n*.pem\nsecrets.json\n"
 const jwtKeySize = 2048
 
 var ConfigInstance *Config
+var SecretsInstance *Secrets
 var JWTPrivateKeyInstance *rsa.PrivateKey
 var BackendJWTPublicKeyInstance *rsa.PublicKey
 
@@ -53,6 +54,16 @@ func Init() error {
 	if _, err := os.Stat(configLocation); os.IsNotExist(err) {
 		config := newConfig()
 		if err := config.Save(); err != nil {
+			return err
+		}
+	}
+
+	if _, err := os.Stat(secretsLocation); os.IsNotExist(err) {
+		secrets, err := newSecrets()
+		if err != nil {
+			return err
+		}
+		if err := secrets.Save(); err != nil {
 			return err
 		}
 	}
@@ -95,6 +106,15 @@ func Init() error {
 		return err
 	}
 	ConfigInstance = config
+
+	secrets, err := loadSecrets()
+	if err != nil {
+		return err
+	}
+	if err := secrets.Migrate(); err != nil {
+		return err
+	}
+	SecretsInstance = secrets
 
 	jwtPrivateKey, err := loadJWTPrivateKey()
 	if err != nil {
@@ -169,6 +189,97 @@ func (c *Config) Save() error {
 	}
 
 	return os.WriteFile(configLocation, data, 0644)
+}
+
+// TODO: Secrets should be stored in HSM when possible, or at least encrypted with the encryption key being in HSM or similar secure storage.
+
+// Secrets values should never be accessed or modified directly as that could lead to race conditions.
+type Secrets struct {
+	lock         sync.RWMutex
+	NodeJTI      string `json:"node_jti"`
+	BackendToken string `json:"backend_token"`
+}
+
+func newSecrets() (*Secrets, error) {
+	return &Secrets{
+		lock:         sync.RWMutex{},
+		NodeJTI:      "", // generated after node is connected to backend
+		BackendToken: "", // set after node is connected to backend
+	}, nil
+}
+
+func loadSecrets() (*Secrets, error) {
+	file, err := os.Open(secretsLocation)
+	if err != nil {
+		if os.IsNotExist(err) {
+			secrets, err := newSecrets()
+			if err != nil {
+				return nil, err
+			}
+			if err := secrets.Save(); err != nil {
+				return nil, err
+			}
+			return secrets, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	var secrets Secrets
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&secrets); err != nil {
+		return nil, err
+	}
+
+	return &secrets, nil
+}
+
+func (s *Secrets) Migrate() error {
+	s.lock.Lock()
+
+	s.lock.Unlock()
+
+	if err := s.Save(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Secrets) Save() error {
+	s.lock.Lock()
+	data, err := json.MarshalIndent(s, "", "  ")
+	s.lock.Unlock()
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(secretsLocation, data, 0600)
+}
+
+func (s *Secrets) GetNodeJTI() string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	return s.NodeJTI
+}
+
+func (s *Secrets) SetNodeJTI(nodeJTI string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.NodeJTI = nodeJTI
+}
+
+func (s *Secrets) GetBackendToken() string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	return s.BackendToken
+}
+
+func (s *Secrets) SetBackendToken(backendToken string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.BackendToken = backendToken
 }
 
 func loadJWTPrivateKey() (*rsa.PrivateKey, error) {
