da44370f09b00a0877fe0364607abb1dd479a59d
mel <mcmelon@nodiumhosting.com>
2025-07-03 15:23:20 +0200

    feat: implement server container creation

--- apps/daemon/internal/server/create.go
+++ apps/daemon/internal/server/create.go
@@ -1,13 +1,81 @@
 package server
 
 import (
+	"context"
 	"fmt"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/network"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"io"
 	"panelium/daemon/internal/db"
+	"panelium/daemon/internal/docker"
 	"panelium/daemon/internal/model"
 	"panelium/proto_gen_go"
 )
 
+func yeetDbServer(sid string) error {
+	if err := db.Instance().Model(&model.ServerAllocation{}).Where("sid = ?", sid).Delete(&model.ServerAllocation{}).Error; err != nil {
+		return fmt.Errorf("failed to delete server allocations: %w", err)
+	}
+
+	if err := db.Instance().Delete(&model.Server{}, "sid = ?", sid).Error; err != nil {
+		return fmt.Errorf("failed to delete server: %w", err)
+	}
+
+	return nil
+}
+
 func CreateServer(sid string, allocations []model.ServerAllocation, resourceLimit model.ResourceLimit, dockerImage string, bid string) (*model.Server, error) {
+	// image pull and container create are done in a goroutine to avoid blocking the main thread and prevent timeouts
+	go func() {
+		rc, err := docker.Instance().ImagePull(context.Background(), dockerImage, image.PullOptions{})
+		if err != nil {
+			if err := yeetDbServer(sid); err != nil {
+				fmt.Printf("failed to rollback server creation: %v\n", err)
+			}
+			fmt.Printf("failed to pull docker image: %v\n", err)
+			return
+		}
+
+		_, err = io.ReadAll(rc)
+		if err != nil {
+			if err := yeetDbServer(sid); err != nil {
+				fmt.Printf("failed to rollback server creation: %v\n", err)
+			}
+			fmt.Printf("failed to read docker image pull response: %v\n", err)
+			return
+		}
+		err = rc.Close()
+		if err != nil {
+			if err := yeetDbServer(sid); err != nil {
+				fmt.Printf("failed to rollback server creation: %v\n", err)
+			}
+			fmt.Printf("failed to close docker image pull response: %v\n", err)
+			return
+		}
+
+		cr, err := docker.Instance().ContainerCreate(context.Background(), &container.Config{}, &container.HostConfig{}, &network.NetworkingConfig{}, &v1.Platform{}, sid)
+		if err != nil {
+			if err := yeetDbServer(sid); err != nil {
+				fmt.Printf("failed to rollback server creation: %v\n", err)
+			}
+			fmt.Printf("failed to create docker container: %v\n", err)
+		}
+
+		if err := docker.Instance().ContainerStart(context.Background(), cr.ID, container.StartOptions{}); err != nil {
+			if err := yeetDbServer(sid); err != nil {
+				fmt.Printf("failed to rollback server creation: %v\n", err)
+			}
+			err := docker.Instance().ContainerRemove(context.Background(), cr.ID, container.RemoveOptions{Force: true})
+			if err != nil {
+				return
+			}
+			fmt.Printf("failed to start docker container: %v\n", err)
+			return
+		}
+	}()
+
 	server := model.Server{
 		SID:           sid,
 		Status:        proto_gen_go.ServerStatusType_SERVER_STATUS_TYPE_INSTALLING,
@@ -41,8 +109,6 @@ func CreateServer(sid string, allocations []model.ServerAllocation, resourceLimi
 			return nil, err
 		}
 	}
-
-	// TODO: docker
 
 	return &server, nil
 }
