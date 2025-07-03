fafbcdee0a127fe728774139dbc6453ff30c5074
mel <mcmelon@nodiumhosting.com>
2025-07-03 23:34:47 +0200

    refactor: make resource usage use floats instead of ints

--- shared/protogen/go/daemon_Server.pb.go
+++ shared/protogen/go/daemon_Server.pb.go
@@ -313,10 +313,10 @@ func (x *ResourceLimit) GetStorage() uint32 {
 
 type ResourceUsage struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
-	Cpu           uint32                 `protobuf:"varint,1,opt,name=cpu,proto3" json:"cpu,omitempty"`         // CPU in percentage (100% = 1 vCore)
-	Ram           uint32                 `protobuf:"varint,2,opt,name=ram,proto3" json:"ram,omitempty"`         // RAM in MB
-	Swap          uint32                 `protobuf:"varint,3,opt,name=swap,proto3" json:"swap,omitempty"`       // SWAP in MB
-	Storage       uint32                 `protobuf:"varint,4,opt,name=storage,proto3" json:"storage,omitempty"` // Storage in MB
+	Cpu           float32                `protobuf:"fixed32,1,opt,name=cpu,proto3" json:"cpu,omitempty"`         // CPU in percentage (100% = 1 vCore)
+	Ram           float32                `protobuf:"fixed32,2,opt,name=ram,proto3" json:"ram,omitempty"`         // RAM in MB
+	Swap          float32                `protobuf:"fixed32,3,opt,name=swap,proto3" json:"swap,omitempty"`       // SWAP in MB
+	Storage       float32                `protobuf:"fixed32,4,opt,name=storage,proto3" json:"storage,omitempty"` // Storage in MB
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -351,28 +351,28 @@ func (*ResourceUsage) Descriptor() ([]byte, []int) {
 	return file_daemon_Server_proto_rawDescGZIP(), []int{2}
 }
 
-func (x *ResourceUsage) GetCpu() uint32 {
+func (x *ResourceUsage) GetCpu() float32 {
 	if x != nil {
 		return x.Cpu
 	}
 	return 0
 }
 
-func (x *ResourceUsage) GetRam() uint32 {
+func (x *ResourceUsage) GetRam() float32 {
 	if x != nil {
 		return x.Ram
 	}
 	return 0
 }
 
-func (x *ResourceUsage) GetSwap() uint32 {
+func (x *ResourceUsage) GetSwap() float32 {
 	if x != nil {
 		return x.Swap
 	}
 	return 0
 }
 
-func (x *ResourceUsage) GetStorage() uint32 {
+func (x *ResourceUsage) GetStorage() float32 {
 	if x != nil {
 		return x.Storage
 	}
@@ -680,10 +680,10 @@ const file_daemon_Server_proto_rawDesc = "" +
 	"\x04swap\x18\x03 \x01(\rR\x04swap\x12\x18\n" +
 	"\astorage\x18\x04 \x01(\rR\astorage\"a\n" +
 	"\rResourceUsage\x12\x10\n" +
-	"\x03cpu\x18\x01 \x01(\rR\x03cpu\x12\x10\n" +
-	"\x03ram\x18\x02 \x01(\rR\x03ram\x12\x12\n" +
-	"\x04swap\x18\x03 \x01(\rR\x04swap\x12\x18\n" +
-	"\astorage\x18\x04 \x01(\rR\astorage\"\xe8\x01\n" +
+	"\x03cpu\x18\x01 \x01(\x02R\x03cpu\x12\x10\n" +
+	"\x03ram\x18\x02 \x01(\x02R\x03ram\x12\x12\n" +
+	"\x04swap\x18\x03 \x01(\x02R\x04swap\x12\x18\n" +
+	"\astorage\x18\x04 \x01(\x02R\astorage\"\xe8\x01\n" +
 	"\x13CreateServerRequest\x12\x1a\n" +
 	"\bserverId\x18\x01 \x01(\tR\bserverId\x124\n" +
 	"\vallocations\x18\x02 \x03(\v2\x12.daemon.AllocationR\vallocations\x12;\n" +
