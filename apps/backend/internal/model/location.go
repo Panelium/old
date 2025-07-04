6d13701841adf666bdc662e72459215cdce09116
mel <mcmelon@nodiumhosting.com>
2025-07-05 00:17:02 +0200

    feat: add string IDs to Node and Location models

--- apps/backend/internal/model/location.go
+++ apps/backend/internal/model/location.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Location struct {
 	gorm.Model
+	LID   string `gorm:"uniqueIndex;not null" json:"lid"`
 	Name  string `gorm:"uniqueIndex;not null" json:"name"`
 	Nodes []Node `gorm:"foreignKey:LocationID" json:"nodes"`
 }
