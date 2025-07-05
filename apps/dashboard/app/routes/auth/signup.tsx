bcf0a8f0541d9e26350c7cbce266b322d8d4cdf5
mel <mcmelon@nodiumhosting.com>
2025-07-05 15:39:25 +0200

    feat: sign up page
    Authored-by: Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>

---
+++ apps/dashboard/app/routes/auth/signup.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+import {
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { useSession } from "~/providers/SessionProvider";
+
+const signupFormSchema = z.object({
+  username: z.string().email({ message: "Username has to be unique" }),
+  email: z.string().email({ message: "Please enter a valid email address" }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
+type SignupFormValues = z.infer<typeof signupFormSchema>;
+
+export default function SignupPage({ onLogin }: { onLogin: () => void }) {
+  const session = useSession();
+  const navigate = useNavigate();
+
+  const defaultValues: SignupFormValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const form = useForm<SignupFormValues>({
+    resolver: zodResolver(signupFormSchema),
+    defaultValues,
+  });
+
+  async function onSubmit(data: SignupFormValues) {
+    // TODO: authenticateUser(data.email, data.password, data.rememberMe);
+    form.setError("root", {
+      message:
+        "Authentication is not implemented yet. Please use the bypass button.",
+    });
+  }
+
+  function bypassAuthentication() {
+    session.setAuthenticated(true);
+    navigate("/", { replace: true });
+  }
+
+  function showSignUpError() {
+    form.setError("root", {
+      message:
+        "Authentication is not implemented yet. Please use the bypass button.",
+    });
+  }
+
+  return (
+    <div className="w-full max-w-sm truncate flex flex-col flex-1/2 gap-4">
+      <CardHeader>
+        <CardTitle className="text-2xl text-center no-select">
+          Create an account
+        </CardTitle>
+        <CardDescription className="text-center no-select">
+          Enter your credentials to access your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="username"
+                      className="no-select"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="email@example.com"
+                      className="no-select"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="••••••••"
+                      className="no-select"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full no-select">
+              Sign Up
+            </Button>
+            {form.formState.errors.root && (
+              <p className="text-sm font-medium text-destructive text-center">
+                {form.formState.errors.root.message}
+              </p>
+            )}
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center">
+        <p className="text-sm text-center text-muted-foreground no-select">
+          Already have an account?{" "}
+          <a
+            className="cursor-pointer text-primary underline-offset-4 hover:underline"
+            onClick={onLogin}
+          >
+            Return to login
+          </a>
+        </p>
+
+        {/*TODO: Remove this once the authentication flow is implemented*/}
+        <Button
+          variant="outline"
+          className="mt-4 w-s no-select"
+          onClick={bypassAuthentication}
+        >
+          bypass authentication
+        </Button>
+      </CardFooter>
+    </div>
+  );
+}
