ec7dd0b30702a6a5587e73917a39a05a6565783a
mel <mcmelon@nodiumhosting.com>
2025-07-06 18:04:07 +0200

    feat: reimplement methods for running commands in the server console and terminal

---
+++ apps/daemon/internal/handler/server/console_command.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"connectrpc.com/connect"
+	"context"
+	"panelium/daemon/internal/security"
+	"panelium/daemon/internal/server"
+	"panelium/proto_gen_go"
+)
+
+func (s *ServerServiceHandler) ConsoleCommand(
+	ctx context.Context,
+	req *connect.Request[proto_gen_go.IDMessage],
+) (*connect.Response[proto_gen_go.Empty], error) {
+	err := security.CheckServerAccess(ctx, req.Msg.Id)
+	if err != nil {
+		return nil, connect.NewError(connect.CodePermissionDenied, err)
+	}
+
+	err = server.ConsoleCommand(req.Msg.Id, req.Msg.Text)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	return nil, nil
+}
