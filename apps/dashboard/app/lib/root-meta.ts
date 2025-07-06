2fd92c9652342dded0e2f82f4def72213941c5d3
mel <mcmelon@nodiumhosting.com>
2025-07-06 23:56:30 +0200

    feat: update meta description

--- apps/dashboard/app/lib/root-meta.ts
+++ apps/dashboard/app/lib/root-meta.ts
@@ -3,7 +3,11 @@ import type { Route } from "../../.react-router/types/app/+types/root";
 export const meta: Route.MetaFunction = () => [
   { charset: "utf-8" },
   { title: "Panelium" },
-  { name: "description", content: "E" },
+  {
+    name: "description",
+    content:
+      "Panelium is an all-in-one server hosting solution that provides a web-based control panel for managing games, servers, applications, and services.",
+  },
   { name: "viewport", content: "width=device-width, initial-scale=1" },
   { name: "theme-color", content: "#ffffff" },
 ];
