b5557c364adaeb4aab18b2ebd365ddf91100f7d0
mel <mcmelon@nodiumhosting.com>
2025-07-06 15:27:34 +0200

    refactor: make NodeToken and BackendJTI in Node db model nullable

--- apps/backend/internal/model/node.go
+++ apps/backend/internal/model/node.go
@@ -17,6 +17,6 @@ type Node struct {
 	MaxRAM      uint             `gorm:"not null" json:"max_ram"`     // Maximum RAM in MB
 	MaxSWAP     uint             `gorm:"not null" json:"max_swap"`    // Maximum Swap in MB
 	MaxStorage  uint             `gorm:"not null" json:"max_storage"` // Maximum Storage in MB
-	NodeToken   string           `json:"node_token"`                  // Encrypted node token (backend->daemon communication)
-	BackendJTI  string           `json:"backend_jti"`                 // JWT ID of the backend token (daemon->backend communication)
+	NodeToken   *string          `json:"node_token"`                  // Encrypted node token (backend->daemon communication)
+	BackendJTI  *string          `json:"backend_jti"`                 // JWT ID of the backend token (daemon->backend communication)
 }
