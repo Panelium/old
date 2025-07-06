a5c4a041ab29a6f91f1ef9552eee9913da5ce2aa
mel <mcmelon@nodiumhosting.com>
2025-07-06 15:58:01 +0200

    feat: implement daemon service method handlers

--- apps/backend/internal/handler/daemon/sync_blueprints.go
+++ apps/backend/internal/handler/daemon/sync_blueprints.go
@@ -4,7 +4,9 @@ import (
 	"connectrpc.com/connect"
 	"context"
 	"errors"
+	"panelium/backend/internal/db"
 	"panelium/backend/internal/middleware"
+	"panelium/backend/internal/model"
 	"panelium/proto_gen_go"
 	"panelium/proto_gen_go/backend"
 )
@@ -18,6 +20,71 @@ func (s *DaemonServiceHandler) SyncBlueprints(
 	daemonInfo, ok := daemonInfoData.(*middleware.DaemonInfo)
 	if !ok || daemonInfo == nil || daemonInfo.NID == "" {
 		return connect.NewError(connect.CodeUnauthenticated, errors.New("invalid node token"))
+	}
+
+	var blueprints []*model.Blueprint
+	tx := db.Instance().Find(&blueprints)
+	if tx.Error != nil {
+		return connect.NewError(connect.CodeInternal, tx.Error)
+	}
+
+	for _, blueprint := range blueprints {
+		var flags []string
+		err := blueprint.Flags.Scan(&flags)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		var blockedFiles []struct {
+			File     string `json:"file"`
+			Visible  bool   `json:"visible"`
+			Readable bool   `json:"readable"`
+		}
+		err = blueprint.BlockedFiles.Scan(&blockedFiles)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		var blockedFilesProto []*backend.BlockedFile
+		for _, file := range blockedFiles {
+			blockedFilesProto = append(blockedFilesProto, &backend.BlockedFile{
+				File:     file.File,
+				Visible:  file.Visible,
+				Readable: file.Readable,
+			})
+		}
+
+		var dockerImages []struct {
+			Name  string `json:"name"`
+			Image string `json:"image"`
+		}
+		err = blueprint.DockerImages.Scan(&dockerImages)
+		if err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
+
+		var dockerImagesProto []string
+		for _, image := range dockerImages {
+			dockerImagesProto = append(dockerImagesProto, image.Image)
+		}
+
+		blueprintProto := &backend.Blueprint{
+			Bid:                    blueprint.BID,
+			Version:                uint32(blueprint.Version),
+			Flags:                  flags,
+			BlockedFiles:           blockedFilesProto,
+			DockerImages:           dockerImagesProto,
+			ServerBinary:           blueprint.ServerBinary,
+			StartCommand:           blueprint.StartCommand,
+			StopCommand:            blueprint.StopCommand,
+			SetupScriptBase64:      blueprint.SetupScriptBase64,
+			SetupDockerImage:       blueprint.SetupDockerImage,
+			SetupScriptInterpreter: blueprint.SetupScriptInterpreter,
+		}
+
+		if err := stm.Send(blueprintProto); err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
 	}
 
 	return nil
--- apps/backend/internal/handler/daemon/sync_servers.go
+++ apps/backend/internal/handler/daemon/sync_servers.go
@@ -4,7 +4,9 @@ import (
 	"connectrpc.com/connect"
 	"context"
 	"errors"
+	"panelium/backend/internal/db"
 	"panelium/backend/internal/middleware"
+	"panelium/backend/internal/model"
 	"panelium/proto_gen_go"
 	"panelium/proto_gen_go/backend"
 )
@@ -18,6 +20,52 @@ func (s *DaemonServiceHandler) SyncServers(
 	daemonInfo, ok := daemonInfoData.(*middleware.DaemonInfo)
 	if !ok || daemonInfo == nil || daemonInfo.NID == "" {
 		return connect.NewError(connect.CodeUnauthenticated, errors.New("invalid node token"))
+	}
+
+	var node *model.Node
+	tx := db.Instance().First(&node, "nid = ?", daemonInfo.NID)
+	if tx.Error != nil || tx.RowsAffected == 0 {
+		return connect.NewError(connect.CodeNotFound, errors.New("node not found"))
+	}
+
+	servers := node.Servers
+
+	for _, server := range servers {
+		var userIds []string
+		for _, user := range server.Users {
+			userIds = append(userIds, user.User.UID)
+		}
+
+		var allocations []*proto_gen_go.IPAllocation
+		for _, allocation := range server.Allocations {
+			if allocation.IP == "" || allocation.Port < 1024 || allocation.Port > 65535 {
+				return connect.NewError(connect.CodeInvalidArgument, errors.New("invalid server allocation"))
+			}
+
+			allocations = append(allocations, &proto_gen_go.IPAllocation{
+				Ip:   allocation.IP,
+				Port: uint32(allocation.Port),
+			})
+		}
+
+		serverProto := backend.Server{
+			Sid:         server.SID,
+			OwnerId:     server.Owner.UID,
+			UserIds:     userIds,
+			Allocations: allocations,
+			ResourceLimit: &proto_gen_go.ResourceLimit{
+				Cpu:     uint32(server.ResourceLimit.CPU),
+				Ram:     uint32(server.ResourceLimit.RAM),
+				Swap:    uint32(server.ResourceLimit.SWAP),
+				Storage: uint32(server.ResourceLimit.Storage),
+			},
+			DockerImage: server.DockerImage,
+			Bid:         server.BID,
+		}
+
+		if err := stm.Send(&serverProto); err != nil {
+			return connect.NewError(connect.CodeInternal, err)
+		}
 	}
 
 	return nil
