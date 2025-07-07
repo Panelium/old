ce82d00b35e50e4a8f9792f5b2d2fa0a89878e0b
mel <mcmelon@nodiumhosting.com>
2025-07-07 11:41:37 +0200

    fix: add admin routes to protected layout

--- apps/dashboard/app/routes.ts
+++ apps/dashboard/app/routes.ts
@@ -1,25 +1,14 @@
-import {
-  index,
-  layout,
-  prefix,
-  route,
-  type RouteConfig,
-} from "@react-router/dev/routes";
+import { index, layout, prefix, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  ...prefix("auth", [
-    layout("layouts/AuthLayout.tsx", [index("routes/auth/index.tsx")]),
-  ]),
-  
-  ...prefix("admin", [
-    layout("layouts/AdminLayout.tsx", [index("routes/admin/index.tsx")])
-  ]),
+  ...prefix("auth", [layout("layouts/AuthLayout.tsx", [index("routes/auth/index.tsx")])]),
 
   layout("layouts/ProtectedLayout.tsx", [
     layout("layouts/UserDashboardLayout.tsx", [
       index("routes/dashboard/index.tsx"),
       route("server/*", "routes/dashboard/server/[id].tsx"),
     ]),
+    ...prefix("admin", [layout("layouts/AdminLayout.tsx", [index("routes/admin/index.tsx")])]),
   ]),
 
   // route("*", "routes/not-found.tsx"),
