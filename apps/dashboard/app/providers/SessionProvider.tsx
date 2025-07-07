621ed623cc20f2b1b26d8e666a0c01bbf3e995f5
mel <mcmelon@nodiumhosting.com>
2025-07-07 10:27:57 +0200

    docs: remove this damn comment for like the fifth time now why does it keep coming back :sob:

--- apps/dashboard/app/providers/SessionProvider.tsx
+++ apps/dashboard/app/providers/SessionProvider.tsx
@@ -6,13 +6,6 @@ interface SessionProviderProps {
   storageKey?: string;
 }
 
-/*
-Cookies: (All cookies are http-only and secure, set by the backend)
-- `refresh_token`: Used to obtain a new JWT when the current one expires. This is stored in a secure, http-only PERSISTENT cookie. TODO-INFO: 24 Hours - 30 days expiration, might be user configurable.
-- `session_id`: Used to identify the session on the backend. This is also stored in a secure, http-only SESSION cookie. TODO-INFO: 5-30 minutes expiration
-- `jwt`: Used to authenticate requests to the backend. This is stored in a secure, http-only SESSION cookie. TODO-INFO: 5-30 minutes expiration
- */
-
 export interface SessionProviderState {
   authenticated: boolean;
   setAuthenticated: (authenticated: boolean) => void;
