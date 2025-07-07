d00c0f4c45a3b48b57ed9a03e2130fe258fde03d
mel <mcmelon@nodiumhosting.com>
2025-07-07 18:19:19 +0200

    feat: add a lazy loaded IfElse function

--- shared/common/go/util/ifelse.go
+++ shared/common/go/util/ifelse.go
@@ -1,8 +1,15 @@
 package util
 
-func IfElse[T any](condition bool, ifTrue T, ifFalse T) T {
-	if condition {
+func IfElse[T any](predicate bool, ifTrue T, ifFalse T) T {
+	if predicate {
 		return ifTrue
 	}
 	return ifFalse
+}
+
+func LazyIfElse[T any](predicate bool, ifTrue func() T, ifFalse func() T) T {
+	if predicate {
+		return ifTrue()
+	}
+	return ifFalse()
 }
