bc1a85eb2e2ba1e94e6a27d19a98a488737687e9
mel <mcmelon@nodiumhosting.com>
2025-07-07 03:04:06 +0200

    feat: add rate limits for login, register and token refresh methods

--- apps/backend/internal/handler/auth/login.go
+++ apps/backend/internal/handler/auth/login.go
@@ -3,21 +3,30 @@ package auth
 import (
 	"connectrpc.com/connect"
 	"context"
+	"fmt"
 	"panelium/backend/internal/db"
 	"panelium/backend/internal/model"
+	"panelium/backend/internal/rate_limit"
 	"panelium/backend/internal/security"
 	"panelium/backend/internal/security/cookies"
 	"panelium/backend/internal/security/session"
 	"panelium/common/errors"
 	"panelium/proto_gen_go/backend"
+	"time"
 )
 
 // TODO: need to add rate limiting
+
+var loginLimiter = rate_limit.NewRateLimiter(10, time.Minute) // 10 requests/minute per IP
 
 func (s *AuthServiceHandler) Login(
 	ctx context.Context,
 	req *connect.Request[backend.LoginRequest],
 ) (*connect.Response[backend.LoginResponse], error) {
+	if !loginLimiter.Allow(req.Peer().Addr) {
+		return nil, connect.NewError(connect.CodeResourceExhausted, fmt.Errorf("too many login attempts, please try again later"))
+	}
+
 	user := &model.User{}
 	tx := db.Instance().First(user, "username = ? OR email = ?", req.Msg.Username, req.Msg.Username)
 	if tx.RowsAffected == 0 || tx.Error != nil {
