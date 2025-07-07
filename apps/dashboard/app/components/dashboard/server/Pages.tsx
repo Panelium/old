587c35992e2c294ac17ad257f9b179985941bd43
mel <mcmelon@nodiumhosting.com>
2025-07-07 19:12:19 +0200

    fix: initial data fetching fixup/prep in server pages

--- apps/dashboard/app/components/dashboard/server/Pages.tsx
+++ apps/dashboard/app/components/dashboard/server/Pages.tsx
@@ -1,100 +1,110 @@
-import { Activity, HardDrive, Settings, Terminal, type LucideProps } from "lucide-react";
-import type React from "react"
-import { useState } from "react";
+import { Activity, HardDrive, type LucideProps, Settings, Terminal } from "lucide-react";
+import type React from "react";
 import { cn } from "~/lib/utils";
 import ConsolePage from "./pages/ConsolePage";
 import ActivityPage from "./pages/ActivityPage";
 import FilesPage from "./pages/FilesPage";
 import SettingsPage from "./pages/SettingsPage";
+import { ServerData } from "~/components/cards/server-card/ServerCard";
 
 export const pagesEventBus = new Comment("event-bus");
 
 export class PagePressedEvent extends Event {
-    #id;
+  #id;
 
-    constructor(id: string) {
-        super("pagePressed");
-        this.#id = id;
-    }
+  constructor(id: string) {
+    super("pagePressed");
+    this.#id = id;
+  }
 
-    get id() {
-        return this.#id;
-    }
+  get id() {
+    return this.#id;
+  }
 }
 
-const transition = "transition-all"
+const transition = "transition-all";
 
 interface PageProps {
-    icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref">>,
-    id: string,
-    currentId: string,
-    color?: string,
+  icon: React.ForwardRefExoticComponent<Omit<LucideProps, "ref">>;
+  id: string;
+  currentId: string;
+  color?: string;
 }
 
-const Page: React.FC<PageProps> = ({icon, id, currentId, color}) => {
-    const IconComponent = icon;
+const Page: React.FC<PageProps> = ({ icon, id, currentId, color }) => {
+  const IconComponent = icon;
 
-    // Weird workaround to get tailwind generating
-    let bg70 = "bg-tag-gray/70";
-    let bg20 = "bg-tag-gray/20";
-    let iconColor = "group-hover/page:text-tag-gray text-tag-gray/70";
-    
-    if (color === "purple") { bg70 = "bg-tag-purple/70"; bg20 = "bg-tag-purple/20"; iconColor = "group-hover/page:text-tag-purple text-tag-purple/70"; }
-    else if (color === "red") { bg70 = "bg-tag-red/70"; bg20 = "bg-tag-red/20"; iconColor = "group-hover/page:text-tag-red text-tag-red/70"; }
-    else if (color === "green") { bg70 = "bg-tag-green/70"; bg20 = "bg-tag-green/20"; iconColor = "group-hover/page:text-tag-green text-tag-green/70"; }
-    else if (color === "orange") { bg70 = "bg-tag-orange/70"; bg20 = "bg-tag-orange/20"; iconColor = "group-hover/page:text-tag-orange text-tag-orange/70"; }
+  // Weird workaround to get tailwind generating
+  let bg70 = "bg-tag-gray/70";
+  let bg20 = "bg-tag-gray/20";
+  let iconColor = "group-hover/page:text-tag-gray text-tag-gray/70";
 
-    return (
-        <button className={cn(
-            "flex items-center h-14 w-14 cursor-pointer group/page",
-            currentId === id ? bg20 : ""
-        )} onClick={() => {
-            pagesEventBus.dispatchEvent(new PagePressedEvent(id));
-        }}>
-            <div className={cn(
-                "h-full w-1 mr-[calc(var(--spacing)*-1)]",
-                bg70, 
-                "transition-all duration-100 ease-linear", 
-                currentId === id ? "scale-x-100" : "scale-x-0"
-            )}/>
-            <IconComponent className={cn(
-                "h-7 w-7 m-auto",
-                transition,
-                currentId === id ? iconColor : "group-hover/page:text-foreground text-foreground/70"
-            )}/>
-        </button>
-    );
-}
+  if (color === "purple") {
+    bg70 = "bg-tag-purple/70";
+    bg20 = "bg-tag-purple/20";
+    iconColor = "group-hover/page:text-tag-purple text-tag-purple/70";
+  } else if (color === "red") {
+    bg70 = "bg-tag-red/70";
+    bg20 = "bg-tag-red/20";
+    iconColor = "group-hover/page:text-tag-red text-tag-red/70";
+  } else if (color === "green") {
+    bg70 = "bg-tag-green/70";
+    bg20 = "bg-tag-green/20";
+    iconColor = "group-hover/page:text-tag-green text-tag-green/70";
+  } else if (color === "orange") {
+    bg70 = "bg-tag-orange/70";
+    bg20 = "bg-tag-orange/20";
+    iconColor = "group-hover/page:text-tag-orange text-tag-orange/70";
+  }
 
-const Pages: React.FC = () => {
-    const [id, setId] = useState(location.pathname.split("/").length > 6 ? location.pathname.split("/")[3] : FilesPage.id);
+  return (
+    <button
+      className={cn("flex items-center h-14 w-14 cursor-pointer group/page", currentId === id ? bg20 : "")}
+      onClick={() => {
+        pagesEventBus.dispatchEvent(new PagePressedEvent(id));
+      }}
+    >
+      <div
+        className={cn(
+          "h-full w-1 mr-[calc(var(--spacing)*-1)]",
+          bg70,
+          "transition-all duration-100 ease-linear",
+          currentId === id ? "scale-x-100" : "scale-x-0"
+        )}
+      />
+      <IconComponent
+        className={cn(
+          "h-7 w-7 m-auto",
+          transition,
+          currentId === id ? iconColor : "group-hover/page:text-foreground text-foreground/70"
+        )}
+      />
+    </button>
+  );
+};
 
-    const FilesPageComponent = FilesPage.component(id);
-    const ConsolePageComponent = ConsolePage.component(id);
-    const ActivityPageComponent = ActivityPage.component(id);
-    const SettingsPageComponent = SettingsPage.component(id);
+export default function Pages({ server }: { server: ServerData }) {
+  const id = server.serverInfo.sid;
 
-    pagesEventBus.addEventListener("pagePressed", (e) => {
-        if (!(e instanceof PagePressedEvent)) { return; }
-        setId(e.id);
-    });
+  const FilesPageComponent = FilesPage.component(id);
+  const ConsolePageComponent = ConsolePage.component(id);
+  const ActivityPageComponent = ActivityPage.component(id);
+  const SettingsPageComponent = SettingsPage.component(id);
 
-    return (
-        <div className="flex felx-row bg-server-card border-border shadow-lg shadow-black/20 rounded-xl overflow-hidden">
-        <div className="flex flex-col items-center w-14 group/holder bg-card overflow-hidden shadow-md shadow-black/10">
-            <Page icon={HardDrive} id={FilesPage.id}    currentId={id} color="orange"/>
-            <Page icon={Terminal}  id={ConsolePage.id}  currentId={id} color="purple"/>
-            <Page icon={Activity}  id={ActivityPage.id} currentId={id} color="green"/>
-            <div className="mt-auto">
-                <Page icon={Settings}  id={SettingsPage.id} currentId={id} color="gray"/>
-            </div>
+  return (
+    <div className="flex felx-row bg-server-card border-border shadow-lg shadow-black/20 rounded-xl overflow-hidden">
+      <div className="flex flex-col items-center w-14 group/holder bg-card overflow-hidden shadow-md shadow-black/10">
+        <Page icon={HardDrive} id={FilesPage.id} currentId={id} color="orange" />
+        <Page icon={Terminal} id={ConsolePage.id} currentId={id} color="purple" />
+        <Page icon={Activity} id={ActivityPage.id} currentId={id} color="green" />
+        <div className="mt-auto">
+          <Page icon={Settings} id={SettingsPage.id} currentId={id} color="gray" />
         </div>
-        <FilesPageComponent/>
-        <ConsolePageComponent/>
-        <ActivityPageComponent/>
-        <SettingsPageComponent/>
-        </div>
-    );
+      </div>
+      <FilesPageComponent />
+      <ConsolePageComponent />
+      <ActivityPageComponent />
+      <SettingsPageComponent />
+    </div>
+  );
 }
-
-export default Pages
--- apps/dashboard/app/routes/dashboard/server/[id].tsx
+++ apps/dashboard/app/routes/dashboard/server/[id].tsx
@@ -1,15 +1,73 @@
 import React from "react";
 import ServerHeader from "~/components/dashboard/server/ServerHeader";
 import Pages from "~/components/dashboard/server/Pages";
+import { useParams } from "react-router";
+import { ServerData } from "~/components/cards/server-card/ServerCard";
+import { Client } from "@connectrpc/connect";
+import { getClientClient, getDaemonServerClient } from "~/lib/api-clients";
+import { ServerService } from "proto-gen-ts/daemon/Server_pb";
+import { ClientService, ServerInfo } from "proto-gen-ts/backend/Client_pb";
 
 export default function ServerDetailsPage() {
-  const { server } = {} as any; // TODO: replace with actual data fetching logic
+  const { id } = useParams<{ id: string }>();
+  if (!id) {
+    return <div className="p-6">Server ID is required.</div>;
+  }
+
+  const [clientClient, setClientClient] = React.useState<Client<typeof ClientService>>();
+  const [serverInfo, setServerInfo] = React.useState<ServerInfo>();
+  const [serverClient, setServerClient] = React.useState<Client<typeof ServerService>>();
+
+  React.useEffect(() => {
+    (async () => {
+      const clientClient = await getClientClient();
+      setClientClient(clientClient);
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    (async () => {
+      if (!clientClient || !id) return;
+
+      const serverInfoResponse = await clientClient.getServer({ id });
+      if (serverInfoResponse) {
+        setServerInfo(serverInfoResponse);
+      } else {
+        console.error("Failed to fetch server data");
+      }
+    })();
+  }, [clientClient, id]);
+
+  const [serverData, setServerData] = React.useState<ServerData>();
+
+  React.useEffect(() => {
+    (async () => {
+      if (!serverInfo) return;
+      const client = await getDaemonServerClient(serverInfo?.daemonHost);
+      setServerClient(client);
+    })();
+  }, [serverInfo]);
+
+  React.useEffect(() => {
+    (async () => {
+      if (!serverClient || !id || !serverInfo) return;
+
+      const serverResourceUsageResponse = await serverClient.resourceUsage({ id });
+      const serverStatusResponse = await serverClient.status({ id });
+    })();
+  }, [serverClient, id, serverInfo]);
 
   return (
     <div className="p-6 pb-16">
       <div className="max-w-7xl mx-auto space-y-4">
-        <ServerHeader server={server} />
-        <Pages />
+        {serverData ? (
+          <>
+            <ServerHeader server={serverData} />
+            <Pages server={serverData} />
+          </>
+        ) : (
+          <div className="text-center text-gray-500">Loading server details...</div>
+        )}
       </div>
     </div>
   );
